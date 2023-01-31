e401de88fe305abd9e2ea50d3722ce8cc0b22089
Oğuzhan Sağoğlu <oguzhan.sagoglu@inventanalytics.com>
2023-01-31 16:09:34 +0300

    update config

--- KubernetesDemo/examples/apps/app_a/docker/app.py
+++ KubernetesDemo/examples/apps/app_a/docker/app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask
 
 from src.about import about
@@ -5,9 +7,16 @@ from src.home import home
 
 
 app = Flask(__name__)
-
-app.register_blueprint(home, url_prefix="/")
+app.register_blueprint(home, url_prefix="/home")
 app.register_blueprint(about, url_prefix="/about")
 
+with open('examples/apps/app_a/app_a.conf.json') as config_file:
+    conf = json.load(config_file)
+
+
+@app.route("/")
+def root():
+    return f"Welcome to {conf.get('name')}"
+
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=8888)
+    app.run("0.0.0.0", port=conf.get("port"))
--- KubernetesDemo/examples/apps/app_b/docker/app.py
+++ KubernetesDemo/examples/apps/app_b/docker/app.py
@@ -1,11 +1,20 @@
+import json
+
 from flask import Flask
 
 from src.home import home
 
 
 app = Flask(__name__)
+app.register_blueprint(home, url_prefix="/home")
 
-app.register_blueprint(home, url_prefix="/")
+with open('examples/apps/app_b/app_b.conf.json') as config_file:
+    conf = json.load(config_file)
+
+
+@app.route("/")
+def root():
+    return f"Welcome to {conf.get('name')}"
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=8889)
+    app.run("0.0.0.0", port=conf.get("port"))
